a028f54d911fd5c9f91e1b952f342688fd762db4
dannycypher <dansarpong.me@gmail.com>
2022-03-25 20:33:38 +0000

    Created initial app file and added beep sound
    Initial app file was created with python which runs on a command line interface. The beep sound (beep.wav) is the alarm sound the app uses.

---
+++ app.py
@@ -0,0 +1,72 @@
+import winsound, time
+
+# sound 1
+def breakBeep():
+    freq = 200
+    dur = 50
+
+    for i in range(5):	
+        winsound.Beep(freq, dur)	
+        freq+= 100
+        dur+= 50
+
+# sound 2
+def nextBeep():
+    freq = 800
+    dur = 500
+
+    for i in range(5):	
+        winsound.Beep(freq, dur)	
+        freq-= 100
+        dur-= 50
+
+# main study schedule
+def main(study=3):
+    #initial 2 minutes prep
+    print("2 minutes to prepare for study session begins!\n")
+    time.sleep(110)
+    winsound.PlaySound("beep.wav", winsound.SND_FILENAME)
+
+    # learning period with break and prep
+    for i in range(study):
+        #learn
+        print("Start first lesson now!\n")
+        time.sleep(1490)
+        winsound.PlaySound("beep.wav", winsound.SND_FILENAME)
+        
+        #break
+        print("4 minutes break...")
+        time.sleep(240)
+        breakBeep()
+        print("Break ends in 1 minute. Get ready!\n")
+        time.sleep(60)
+        nextBeep()
+
+    # recalling period
+    for i in range(study):
+        # Recall session
+        print("Start first recall session now!\n")
+        time.sleep(1490)
+        winsound.PlaySound("beep.wav", winsound.SND_FILENAME)
+        
+        #break
+        while i != (study-1):
+            print("5 minutes break...\n")
+            time.sleep(300)
+            nextBeep()
+
+# main study app
+try:
+    # Use default time or set time manually
+    default_user = str(input("Do you want to use the default 3-hour study schedule? (y/n)\n"))
+    if default_user.lower() in ["y", "yes"]:
+        main()
+    else:
+        study_hours = int(input("How many study hours for today? "))
+        print("Good stuff! Let's begin!")
+        time.sleep(2)
+        main(study_hours)
+except:
+    print("Error!")
+
+exit()
